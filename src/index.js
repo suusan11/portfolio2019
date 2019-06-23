625a8bc67566c48b985a56cf2510ba268f2ca275
suusan11 <gawow11@gmail.com>
2019-06-23 16:54:29 -0700

    add header animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./scss/style.scss";
+
+//header fix --> get scroll position
+window.addEventListener('scroll', function(){
+    const scrollValue = window.scrollY;
+    const headerHeight = document.getElementById('js-header').clientHeight;
+    const scrollTotal = scrollValue + headerHeight;
+
+    const breakPoint = document.getElementById('js-breakPoint').clientHeight;
+    const objectTop = document.getElementById('js-breakPoint').offsetTop;
+    const breakPointTotal = breakPoint + objectTop;
+
+    const headerShow = document.getElementById('js-header');
+
+    if(scrollTotal > breakPointTotal) {
+        headerShow.classList.add('is-show');
+    }else {
+        headerShow.classList.remove('is-show');
+    }
+});
+
