0fb5ee673f725b6c6e425c5ac25dc00d4fea34b5
suusan11 <gawow11@gmail.com>
2019-07-05 19:23:57 -0700

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 // [定数] webpack の出力オプションを指定します
 // 'production' か 'development' を指定
 const MODE = 'development';
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // ソースマップの利用有無(productionのときはソースマップを利用しない)
 const enabledSourceMap = (MODE === 'development');
 
@@ -10,9 +10,13 @@ module.exports = {
     // development に設定するとソースマップ有効でJSファイルが出力される
     mode: MODE,
 
-    devServer: {
-        contentBase: "dist",
-        open: true
+    entry: './src/index.js',
+    // ファイルの出力設定
+    output: {
+        //  出力ファイルのディレクトリ名
+        path: `${__dirname}/dist`,
+        // 出力ファイル名
+        filename: 'main.js'
     },
 
     module: {
@@ -20,48 +24,47 @@ module.exports = {
             // Sassファイルの読み込みとコンパイル
             {
                 test: /\.scss/, // 対象となるファイルの拡張子
-                // ローダー名
-                use: [
-                    // linkタグに出力する機能
-                    'style-loader',
-                    // CSSをバンドルするための機能
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            // オプションでCSS内のurl()メソッドを取り込む
-                            url: true,
-                            // ソースマップの利用有無
-                            sourceMap: enabledSourceMap,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        // CSSをバンドルするための機能
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // オプションでCSS内のurl()メソッドを取り込む
+                                url: true,
+                                // ソースマップの利用有無
+                                sourceMap: enabledSourceMap,
 
-                            // 0 => no loaders (default);
-                            // 1 => postcss-loader;
-                            // 2 => postcss-loader, sass-loader
-                            importLoaders: 2
+                                // 0 => no loaders (default);
+                                // 1 => postcss-loader;
+                                // 2 => postcss-loader, sass-loader
+                                importLoaders: 2
+                            },
                         },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            // PostCSS側でもソースマップを有効にする
-                            sourceMap: true,
-                            plugins: [
-                                // Autoprefixerを有効化
-                                // ベンダープレフィックスを自動付与する
-                                require("autoprefixer")({
-                                    grid: true
-                                })
-                            ]
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                // PostCSS側でもソースマップを有効にする
+                                sourceMap: true,
+                                plugins: [
+                                    // Autoprefixerを有効化
+                                    // ベンダープレフィックスを自動付与する
+                                    require("autoprefixer")({
+                                        grid: true
+                                    })
+                                ]
+                            }
+                        },
+                        // Sassをバンドルするための機能
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                // ソースマップの利用有無
+                                sourceMap: enabledSourceMap,
+                            }
                         }
-                    },
-                    // Sassをバンドルするための機能
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            // ソースマップの利用有無
-                            sourceMap: enabledSourceMap,
-                        }
-                    }
-                ],
+                    ],
+                }),
             },
             {
                 test: /\.(wof|woff|woff2|ttf)$/,
@@ -78,4 +81,7 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+    ],
 };
