c46ef04e5f78d9cf115b05dd1878131a046767fa
suusan11 <gawow11@gmail.com>
2019-07-29 14:56:46 -0700

    fix animation to values section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./scss/style.scss";
 import * as commonFiles  from './common';
-import * as animationFiles from './animation';
+// import * as animationFiles from './animation';
 
 //loading after read HTML
 window.onload = function() {
     commonFiles.globalMenu();
     commonFiles.sendTop();
     commonFiles.smoothScroll();
-    animationFiles.valuesAnimation();
+    // animationFiles.valuesAnimation();
 };
